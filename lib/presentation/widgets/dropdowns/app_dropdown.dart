c2077c1783ba1a16426a27510333e2856b6f298b
Faysal Zaman <123411654+faysalzaman@users.noreply.github.com>
2025-05-17 14:46:06 +0500

    Updated

--- lib/presentation/widgets/dropdowns/app_dropdown.dart
+++ lib/presentation/widgets/dropdowns/app_dropdown.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
 import 'package:groute_nartec/core/constants/app_colors.dart';
 
@@ -388,7 +390,7 @@ class _MultiSelectAnimatedDropdownState<T>
                     ),
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
+                        color: Colors.black.withValues(alpha: 0.1),
                         blurRadius: 4,
                         offset: const Offset(0, 2),
                       ),
