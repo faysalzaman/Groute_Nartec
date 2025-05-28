8cc98bd0e3f2387a4ed14044a89fde2ec9aea896
Wasim Zaman <wasimxaman13@gmail.com>
2025-05-28 14:44:20 +0500

    updated

--- lib/presentation/modules/dashboard/sales_order/view/screens/new_orders/action_screens/capture_images/display_products_screen.dart
+++ lib/presentation/modules/dashboard/sales_order/view/screens/new_orders/action_screens/capture_images/display_products_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:groute_nartec/core/utils/app_navigator.dart';
